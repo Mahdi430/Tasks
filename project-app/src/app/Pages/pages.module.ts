fea7bdb6406ac9ea4aa91cad639fcd3571355954
unknown <10220080@mu.edu.lb>
2024-02-28 01:41:45 +0200

    Edittask component

--- project-app/src/app/Pages/pages.module.ts
+++ project-app/src/app/Pages/pages.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { TaskListComponent } from '../Components/Task-list/task-list.component';
-import { AddtaskComponent } from '../Components/Adding-task/addtask.component';
-import { EditTaskComponent } from '../Components/Edittask/Edittask.component';
+import { TaskListComponent } from './Components/Task-list/task-list.component';
+import { AddtaskComponent } from './Components/Adding-task/addtask.component';
+import { EditTaskComponent } from './Components/Edittask/Edittask.component';
 
 
 export const routes: Routes = [
--- project-app/src/app/app.routes.ts
+++ project-app/src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-
-import { HeaderComponent } from './Layout/header/header.component';
-import { PagesModule } from './Pages/pages/pages.module';
 import { LayoutComponent } from './Layout/Layoutcomponent/layout.component';
 
 
@@ -13,7 +10,7 @@ export const routes: Routes = [
       {
         path: '',
         loadChildren: () =>
-          import("./Pages/pages/pages.module").then((m) => m.PagesModule),
+          import("./Pages/pages.module").then((m) => m.PagesModule),
       },
     ],
   },
